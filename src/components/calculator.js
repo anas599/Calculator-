8b5fc29288146b06c6885ea82b19c442ca863606
anas599 <anas599@yahoo.com>
2023-02-13 21:01:27 +0300

    Add main compononets

---
+++ src/components/calculator.js
@@ -0,0 +1,71 @@
+function mainCalculator() {
+  return (
+    <div className="main">
+      <p id="total">0</p>
+      <div className="mainKeyPad">
+        <button type="button" className="greyKey">
+          AC
+        </button>
+        <button type="button" className="greyKey">
+          +/-
+        </button>
+        <button type="button" className="greyKey">
+          %
+        </button>
+        <button type="button" className="signs">
+          ÷
+        </button>
+        <button type="button" className="greyKey">
+          7
+        </button>
+        <button type="button" className="greyKey">
+          8
+        </button>
+        <button type="button" className="greyKey">
+          9
+        </button>
+        <button type="button" className="signs">
+          X
+        </button>
+        <button type="button" className="greyKey">
+          4
+        </button>
+        <button type="button" className="greyKey">
+          5
+        </button>
+        <button type="button" className="greyKey">
+          6
+        </button>
+        <button type="button" className="signs">
+          -
+        </button>
+        <button type="button" className="greyKey">
+          1
+        </button>
+        <button type="button" className="greyKey">
+          2
+        </button>
+        <button type="button" className="greyKey">
+          3
+        </button>
+        <button type="button" className="signs">
+          +
+        </button>
+        <button type="button" className="greyKey extra1">
+          {' '}
+        </button>
+        <button type="button" className="greyKey extra2">
+          0
+        </button>
+        <button type="button" className="greyKey">
+          .
+        </button>
+        <button type="button" className="signs">
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default mainCalculator;
