7bc01436a6bc780f64792000027f07b3cb9b5799
Dennis Every <dennis@ansyra.com>
2023-02-22 16:55:46 -0400

    Add test Button snapshot

---
+++ src/components/___tests___/Button.test.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from 'react-test-renderer';
+import Button from '../Button';
+
+describe('snapshot testing', () => {
+  it('first snapshot', () => {
+    const itemRender = renderer.create(<Button />).toJSON();
+    expect(itemRender).toMatchSnapshot();
+  });
+});
