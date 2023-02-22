093408d24ae85bf60fa35babe4f3d1813350c45d
anas599 <anas599@yahoo.com>
2023-02-22 23:49:39 +0300

    Add the rest of the test

--- src/logic/__tests__/calculate.test.js
+++ src/logic/__tests__/calculate.test.js
@@ -17,7 +17,7 @@ describe('test calculate function', () => {
     };
     expect(calculate(obj2, '0')).toEqual({});
   });
-  test('If there is an operation, update next', ()=> {
+  test('If there is an operation, update next', () => {
     const obj3 = {
       total: null,
       next: '0',
@@ -37,6 +37,78 @@ describe('test calculate function', () => {
     expect(calculate(obj4, '7')).toEqual({
       total: null,
       next: '57',
+    });
+  });
+  test('If there is a (.)', () => {
+    const obj5 = {
+      total: null,
+      next: '0.5',
+      operation: '+',
+    };
+    expect(calculate(obj5, '.')).toEqual({
+      total: null,
+      next: '0.5',
+      operation: '+',
+    });
+  });
+  test('If there is a equal sign (=)', () => {
+    const obj6 = {
+      total: '1',
+      next: '0.5',
+      operation: '+',
+    };
+    expect(calculate(obj6, '=')).toEqual({
+      total: '1.5',
+      next: null,
+      operation: null,
+    });
+  });
+  test('If there is a equal plus or minus (+/-)', () => {
+    const obj7 = {
+      total: '1',
+      next: '5',
+      operation: '+/-',
+    };
+    expect(calculate(obj7, '+/-')).toEqual({
+      total: '1',
+      next: '-5',
+      operation: '+/-',
+    });
+  });
+  test('User pressed an operation after pressing (=)', () => {
+    const obj8 = {
+      total: '1',
+      next: '5',
+      operation: '+',
+    };
+    expect(calculate(obj8, '+')).toEqual({
+      total: '6',
+      next: null,
+      operation: '+',
+    });
+  });
+  test('User pressed an operation button and there is an existing operatio', () => {
+    const obj9 = {
+      total: '5',
+      next: '5',
+      operation: '+',
+    };
+    expect(calculate(obj9, '-')).toEqual({
+      total: '10',
+      next: null,
+      operation: '-',
+    });
+  });
+  test("save the operation and shift 'next' into 'total", () => {
+    const obj10 = {
+      total: null,
+      next: '1',
+      operation: null,
+    };
+    expect(calculate(obj10, '-')).toEqual({
+      total: '1',
+      next: null,
+      operation: '-',
     });
   });
 });
